8c7e0cb211ca1ba8fc354aace59274570f15f9ea
uhhidontknow <1Letter1#>
2019-03-04 09:23:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/keith_meyer/soundboard3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.keith_meyer.soundboard3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
